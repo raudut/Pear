8f7d2fec5dd84932374adb14eaca1293c0b7a6c1
azsky <62022431+azsky@users.noreply.github.com>
2020-06-02 23:47:54 +0200

    Undo Drop lender

--- src/Migrations/Version20200506075315.php
+++ src/Migrations/Version20200506075315.php
@@ -22,7 +22,7 @@ final class Version20200506075315 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        //$this->addSql('DROP TABLE lender');
+        $this->addSql('DROP TABLE lender');
         $this->addSql('ALTER TABLE product ADD owner_id INT NOT NULL');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_D34A04AD7E3C61F9 ON product (owner_id)');
@@ -33,7 +33,7 @@ final class Version20200506075315 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        //$this->addSql('CREATE TABLE lender (id INT AUTO_INCREMENT NOT NULL, iduser_id INT NOT NULL, UNIQUE INDEX UNIQ_AD80FA81786A81FB (iduser_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE lender (id INT AUTO_INCREMENT NOT NULL, iduser_id INT NOT NULL, UNIQUE INDEX UNIQ_AD80FA81786A81FB (iduser_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE lender ADD CONSTRAINT FK_AD80FA81786A81FB FOREIGN KEY (iduser_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD7E3C61F9');
         $this->addSql('DROP INDEX IDX_D34A04AD7E3C61F9 ON product');
