660d8147b860dcafbe19158b2af5690265b7d723
Roxanne <rauduteau@gmail.com>
2020-06-05 23:01:31 +0200

    bj

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -61,6 +61,9 @@ class ClientController extends AbstractController
             'choices'  => [
               $user->getRolesNames()
             ],
+            'multiple' => false,
+            'label' => false,
+            'placeholder' => false
         ],
     ]);
       
