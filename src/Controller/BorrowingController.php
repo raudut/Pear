c7a1e14ec80e7e89a486051d054cc20421057e4c
Roxanne <rauduteau@gmail.com>
2020-06-06 22:10:35 +0200

    rendu 5

--- src/Controller/BorrowingController.php
+++ src/Controller/BorrowingController.php
@@ -149,7 +149,7 @@ class BorrowingController extends AbstractController
 
     public function delete_borrowing(BorrowingRepository $borrowingRepository, $id)
     {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $this->denyAccessUnlessGranted('ROLE_LENDER');
 
         try {
             $bo = $borrowingRepository -> findOneById($id);
